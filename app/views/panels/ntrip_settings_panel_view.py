4838351c8c824aba9d0c60ae6af60817d217714f
Zeki411 <nguyenhatrung411@gmail.com>
2025-05-08 12:31:43 +0000

    fix: enhance button styling and layout in NTRIP settings panel

--- app/views/panels/ntrip_settings_panel_view.py
+++ app/views/panels/ntrip_settings_panel_view.py
@@ -60,12 +60,18 @@ class NTRIPSettingsPanelView(QWidget):
         self.ntrip_password_le.lbl_value.setEchoMode(QLineEdit.EchoMode.Password)
         
         self.save_btn = QPushButton("Save")
-        self.save_btn.setStyleSheet("background-color: #4CAF50; color: white;")
+        self.save_btn.setStyleSheet(
+            "font-weight: bold; font-size: 16px; background-color: #4CAF50; color: white;"
+        )
         self.save_btn.setFixedWidth(100)
+        self.save_btn.setFixedHeight(50)
         
         self.sync_btn = QPushButton("Sync")
-        self.sync_btn.setStyleSheet("background-color: #2196F3; color: white;")
+        self.sync_btn.setStyleSheet(
+            "font-weight: bold; font-size: 16px; background-color: #2196F3; color: white;"
+        )
         self.sync_btn.setFixedWidth(100)
+        self.sync_btn.setFixedHeight(50)
         
         self._init_ui()
 
@@ -74,8 +80,12 @@ class NTRIPSettingsPanelView(QWidget):
         layout.addWidget(self.ntrip_username_le)
         layout.addWidget(self.ntrip_password_le)
         layout.addSpacing(10)
-        layout.addWidget(self.save_btn)
-        layout.addWidget(self.sync_btn)
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(self.sync_btn)
+        btn_layout.addSpacing(10)
+        btn_layout.addWidget(self.save_btn)
+        btn_layout.addStretch(1)
+        layout.addLayout(btn_layout)
         layout.addStretch(1)
         self.setLayout(layout)
         
@@ -102,6 +112,5 @@ class NTRIPSettingsPanelView(QWidget):
                     f"Please fill in the {key} field.",
                 )
                 return None
-        
-        
+            
         return params
